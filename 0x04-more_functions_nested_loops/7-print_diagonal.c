faac948ec2aecfc2de0b3e2fba3e43ceedb7c4d2
SakhileStosh <sithebe.sakhile@gmail.com>
2023-02-23 15:06:14 -0800

    I feel like

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,26 +2,30 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * @n: input number
+ * Return: diagonal
  */
 void print_diagonal(int n)
 {
+
+int co, sp;
+
 if (n <= 0)
 {
 _putchar('\n');
-} else
-{
-int i, j;
+}
 
-for (i = 0; i < n; i++)
+else
 {
-for (j = 0; j < n; j++)
+
+for (co = 1; co <= n; co++)
 {
-if (j == i)
-_putchar('\\');
-else if (j < i)
+
+for (sp = 1; sp < co; sp++)
+{
 _putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
 }
 }
