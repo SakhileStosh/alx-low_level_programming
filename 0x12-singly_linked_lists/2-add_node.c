4757023d7992bb3d09a9376f30018c736be9cf2f
SakhileStosh <sithebe.sakhile.com>
2023-03-30 09:13:23 -0700

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
  * Return: the address of the new element, or NULL if it fails
  */
 list_t *
-add_node (list_t **head, const char *str)
+add_node(list_t **head, const char *str)
 {
 list_t *new;
 unsigned int len = 0;
@@ -18,11 +18,11 @@ unsigned int len = 0;
 while (str[len])
 len++;
 
-new = malloc (sizeof (list_t));
+new = malloc(sizeof(list_t));
 if (!new)
 return (NULL);
 
-new->str = strdup (str);
+new->str = strdup(str);
 new->len = len;
 new->next = (*head);
 (*head) = new;
