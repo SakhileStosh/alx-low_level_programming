3fa0268b61a831748884cc37dac48e50679b8794
SakhileStosh <sithebe.sakhile.com>
2023-03-30 09:06:07 -0700

    list_len

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -9,7 +9,7 @@
  * Return: Always 0.
  */
 int
-main (void)
+main(void)
 {
 list_t *head;
 list_t *new;
@@ -17,19 +17,19 @@ list_t hello = { "World", 5, NULL };
 size_t n;
 
 head = &hello;
-new = malloc (sizeof (list_t));
+new = malloc(sizeof(list_t));
 if (new == NULL)
 {
-printf ("Error\n");
+printf("Error\n");
 return (1);
 }
-new->str = strdup ("Hello");
+new->str = strdup("Hello");
 new->len = 5;
 new->next = head;
 head = new;
-n = list_len (head);
-printf ("-> %lu elements\n", n);
-free (new->str);
-free (new);
+n = list_len(head);
+printf("-> %lu elements\n", n);
+free(new->str);
+free(new);
 return (0);
 }
