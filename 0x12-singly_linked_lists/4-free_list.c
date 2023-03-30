91f58442bf247a181b5b6165d7849aa08a98360c
SakhileStosh <sithebe.sakhile.com>
2023-03-30 09:43:04 -0700

    free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,15 +6,15 @@
  * @head: list_t list to be freed
  */
 void
-free_list (list_t *head)
+free_list(list_t *head)
 {
 list_t *temp;
 
 while (head)
 {
 temp = head->next;
-free (head->str);
-free (head);
+free(head->str);
+free(head);
 head = temp;
 }
 }
